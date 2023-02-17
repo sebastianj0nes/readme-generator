d8390109c335c3f0d69456412c82f85fde8471dc
Seb <jonesseb10@gmail.com>
2023-02-17 18:40:14 +0000

    create initial questions to test inquirer works

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-
+  
 // array of questions for user
 const questions = [
-    
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of the repository?',
+            name: 'repoName',
+          },
+          {
+            type: 'input',
+            message: 'Enter a short description of the project',
+            name: 'description',
+          },
+          {
+            type: 'input',
+            message: 'Enter the installation instructions',
+            name: 'installation',
+        }
+    ]).then((response) => {
+            console.log(response);
+            
+        })
+
 ];
 
 // function to write README file
