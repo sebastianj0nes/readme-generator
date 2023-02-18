fa8b01ca0ec80c5d894909ede6114a1ad245b624
Seb <jonesseb10@gmail.com>
2023-02-18 19:51:51 +0000

    add questions to get input, finish functions to start generator

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-  
+
 // array of questions for user
-const questions = [
+const questions = () => 
     inquirer.prompt([
         {
             type: 'input',
@@ -18,24 +18,59 @@ const questions = [
           },
           {
             type: 'input',
-            message: 'Enter the installation instructions',
+            message: 'Enter the installation instructions?',
             name: 'installation',
+        },
+        {
+            type:"input",
+            message:"What are the usage requirements?",
+            name:"usage"
+        },
+        {
+            type:"input",
+            message:"What are the contribution guidelines?",
+            name:"contribution"
+        },
+        {
+            type:"input",
+            message:"What are the test instructions?",
+            name:"testing"
+        },
+        {
+            type:"list",
+            message:"What license are you using?",
+            name:"license",
+            choices: ['MIT', 'Apache','GPU']
+        },
+        {
+            type:"input",
+            message:"What is your github profile name?",
+            name:"github"
+        },
+        {
+            type:"input",
+            message:"What is your email address?",
+            name:"email"
         }
-    ]).then((response) => {
-            console.log(response);
-            
-        })
-
-];
+    ]);
 
 // function to write README file
 function writeToFile(fileName, data) {
-
+    // Using 'fs' library, write to filename with data
+    fs.writeFile(fileName, data, function (err) {
+        // If there's an error - throw the error
+        if (err) throw err;
+        // Else console log saved
+        console.log(fileName + " has been saved!");
+      });
 }
 
 // function to initialize program
 function init() {
-
+    // Run questions function to get user input
+    questions()
+    // Then with the returned data, write to a readme file using generate markdown function
+    .then((data) => writeToFile('README.md', generateMarkdown(data)))
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,10 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+return `# ${data.repoName}
 
-`;
+`
 }
 
 module.exports = generateMarkdown;
+
+// When a user clicks on the links in the Table of Contents, they are taken to the corresponding section of the README.
